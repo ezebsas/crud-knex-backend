8a787189a56c9fdaf797f8814353e987b95f8bf4
eze <mollinedoezequiel@gmail.com>
2020-02-14 17:27:30 -0300

    creo elementos con POST /todo

---
+++ README.md
@@ -0,0 +1,21 @@
+# Crear aplicacion Full Stack: CRUD todo
+
+La aplicación usa:
+* postgres para la base de datos
+* knex.js para las migraciones, relleno y consultas de base de datos
+* express.js para las rutas y visualizacion
+* Bootstrap para la presentación
+
+## Check List Full Stack
+* [x] Generar app express
+* [x] Crear base de datos
+* [x] Rellenar tablas con datos de ejemplo
+* [x] Listar todos los registros con GET /todo
+* [x] Agregar Bootstrap
+* [x] Mostrar formulario new con /todo/new
+* [x] Crear un registro con POST /todo
+* [ ] Mostrar un registro con GET /todo/:id
+* [ ] Mostrar un formulario de edicion con GET /todo/:id/edit
+* [ ] Actualizar un registro con PUT /todo:id
+* [ ] Eliminar un registro con DELETE /todo/:id
+* [ ] Redirigir al crear / actualizar / eliminar
--- routes/todo.js
+++ routes/todo.js
@@ -1,6 +1,5 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 const knex = require('../db/knex');
 
 /* This router is mounted at /todo */
@@ -11,4 +10,41 @@ router.get('/', (req, res) => {
       res.render('all', { todos: todos });
     });
 });
+
+router.get('/new', (req, res) => {
+  res.render('new');
+});
+
+router.post('/', (req, res) => {
+  if(validTodo(req.body)){
+    const todo = {
+      title: req.body.title,
+      description: req.body.description,
+      priority: req.body.priority,
+      date: new Date()
+    };
+    //insert into the database
+    knex('todo')
+      .insert(todo, 'id')
+      .then(ids => {
+        const id = ids[0];
+        res.redirect(`/todo/${id}`);
+      })
+  }else{
+    //respond with an error
+    res.status(500);
+    res.render('error', {
+      message: 'Invali todo'
+    });
+  }
+});
+
+function validTodo(todo) {
+  return typeof todo.title == 'string' &&
+          todo.title.trim() != '' &&
+          typeof todo.priority != 'undefined' &&
+          !isNaN(todo.priority);
+}
+
+
 module.exports = router;
